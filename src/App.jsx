54ca658714ba7537e196c59eba6388f761214548
Bhargav Jagani <jaganibhargav30@gmail.com>
2025-02-20 14:17:16 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Nabbar from './components/Nabbar'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+        <BrowserRouter>
+        <Nabbar/>
+          <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+          </Routes>
+        </BrowserRouter>
+    </div>
   )
 }
